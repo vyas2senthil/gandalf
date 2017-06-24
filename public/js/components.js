fb1df3664c21dbe0464014554cbaed2a2a40bf52
rajangdavis <rajangdavis@gmail.com>
2017-06-23 20:51:03 -0700

    added in the first section for explaining how to upload a CSV

--- public/js/components.js
+++ public/js/components.js
@@ -1,5 +1,22 @@
 'use strict';
 
+// Component to link back home
+// string currentRoute is checks if the current route is at the root
+var returnHome = function(currentRoute){
+    if( currentRoute!=='/'){
+        return {
+            view: function(){
+                return m('div',{'class':'top-left link',onclick:function(){ return m.route('/')}},[
+                    m('i',{'class':'fa fa-home fa-2x','aria-hidden':'true'}),
+                    m('span',{'class':'lead'},'Home'),
+                ])
+            }
+        }
+    }
+}
+
+
+
 // Component for Links on first page
 // string linkRoute is the route for the link
 // object || string linkText is for the link text or an object to be passed in
@@ -7,7 +24,7 @@
 var linkComponent = function(linkRoute,linkText){
     return {
         view: function(ctrl,args){
-            return m('div',{},[
+            return m('div',{'class':'link'},[
                 m('span',{onclick:function(){ return m.route(linkRoute)}},linkText)
             ])
         }
@@ -23,7 +40,7 @@ var linkComponent = function(linkRoute,linkText){
 var panelComponent = function(panelTitle,iconClass,linkComponent,index){
     return {
         view: function(ctrl,args){
-            return  m('div',{'class':'col-lg-4 col-md-4 col-sm-12 col-xs-12 panel panel-default'},[
+            return  m('div',{'class':'col-lg-4 col-md-4 col-sm-12 col-xs-12 panel panel-default panel-color'},[
                         m('h2',{'class':'panel-body'},[
                             m('span',{'class':'big badge'},index),
                             panelTitle
--- public/js/pages/uploadYourCsv.js
+++ public/js/pages/uploadYourCsv.js
@@ -1,16 +1,47 @@
 'use strict';
+// Array to generate table component
+var numberArr = [1,2,3,4];
+
+// CSV Table example
+
+var csvTable = {
+    view: function(){
+        return m('pre',{'class':'container panel panel-default'},[
+                m('p',{},'"Product Name","Product Parent"'),
+                numberArr.map(function(num){
+                    return numberArr.map(function(num2){
+                        var productLabel = function(num2,num){
+                            return '"Product ' + num2 + ' Level ' + num + '"'
+                        }
+                        var parentLvl = parseInt(num) - 1 == 0 ? '""' : productLabel(parseInt(num2),parseInt(num)-1);
+                        return m('p',{'class':'lvl' + num},[
+                            m('span',{},(productLabel(num2,num)+','+parentLvl)),
+                        ])
+                    })
+                }),
+            ])
+    }
+}
 
 var uploadYourCsv = linkComponent('/uploadYourCsv',
-	'Upload a CSV with the list of products or categories ordered from the lowest level to the highest level.');
+    'Upload a CSV with the list of products or categories ordered from the lowest level to the highest level.');
 
 var uploadYourCsvInfo = {
     view: function(ctrl, args) {
         return m('div',{'class':'container text-center fade-in'}, [
+            returnHome(m.route()),
             m('h1',{},'Upload your CSV'),
-            m('br'),m('br'),m('br'),m('br'),m('br'),m('br'),
-            m('div',{'class':'container-fluid'},[
-                m('p',{},'sup')
-            ])
+            m('br'),m('br'),
+            m('div',{'class':'container-fluid text-left lead'},[
+                m('p',{},'First, you must put together a CSV with the list of products/categories ordered from the lowest level to the highest level. This is to ensure that the products/categories are created in the correct order.'),
+                m('br'),
+                m('p',{},'For example, if you you had 4 levels of products to import, the following is how the CSV should be laid out:'),
+                csvTable,
+                m('p',{},'All that is required is that products/categories are named, listed in the correct order, and reference the name of a parent product or category when applicable.'),
+                m('br'),
+                m('h2',{'class':'link underline text-center',onclick:function(){ return m.route('/verify_your_results')}},'Next: Verify your results')
+            ]),
         ])
     }
 }
+
